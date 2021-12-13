9837482027977473bc83aeee6c009fa92faa1260
developerachem <developerachem@gmail.com>
2021-12-13 02:10:32 -0800

    Add

--- Class 8/Assignment/app.js
+++ Class 8/Assignment/app.js
@@ -1,12 +1,73 @@
-
-let date = new Date() ;
-
+let date = new Date();
 
 
-console.log(`
-
-Time     :        ${(date.getHours() > 12 ? 0 + date.getHours() -12 : date.getHours())} : ${date.getMinutes()} : ${date.getSeconds()} ${ date.getHours() > 12 ? 'PM' : 'AM' }
-
-`);
 
 
+const amin = ['alo', 'potol', 'begun', 'korla', 'gajor',
+
+    {
+        name: 'Abu sayeed',
+        age: 40,
+        skill: ['php', 'java', 'mongoDB', 'python',
+
+            {
+                name: 'mamun',
+                age: 55,
+            }
+        ]
+    }
+]
+
+
+
+
+console.log(` Mamun Vair Heda : ${ amin[5].skill[4].age } Years `);
+
+
+
+
+
+// function customDay() {
+//     if (date.getHours() > 12) {
+//         return date.getHours() - 12;
+
+//     }else{
+//         return date.getHours()
+//     }
+// }
+
+// function finelDay() {
+//     if (customDay() < 10  ) {
+//         return '0' + customDay();
+//     }
+// }
+
+// document.getElementById("time").innerHTML = (`
+
+// Date     :        ${date.getDate()} / ${date.getMonth() + 1} / ${date.getUTCFullYear()}
+
+// <br>
+
+// Time     :        ${finelDay()} : ${(date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes())} : ${(date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds())} ${ date.getHours() > 12 ? 'PM' : 'AM' }
+
+// `);
+
+
+
+
+// console.log(`
+
+// Date     :        ${date.getDate()} / ${date.getMonth() + 1} / ${date.getUTCFullYear()}
+
+// Time     :        ${finelDay()} : ${(date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes())} : ${(date.getSeconds() > 9 ? date.getSeconds() : '0' + date.getSeconds())} ${ date.getHours() > 12 ? 'PM' : 'AM' }
+
+// `);
+
+
+
+
+// console.log( `
+
+// My Time : ${ 31 -  date.getDate() } Day / ${ 23 - date.getHours()  } Hours / ${ date.getMinutes() - 59 } Minit / ${ 0 - date.getSeconds() }
+
+// `);
---
+++ Class 9/app.js
@@ -0,0 +1,268 @@
+const phone = [
+    {
+        name        : 'MD Amzad' ,
+        relation    : 'Brother' ,
+        location    : 'Charfation' ,
+        phoneNumber : '01728104511' ,
+    },
+    {
+        name        : 'MD Azmir' ,
+        relation    : 'Brother' ,
+        location    : 'Singapur' ,
+        phoneNumber : '16852369' ,
+    },
+    {
+        name        : 'Kamrun Nahar' ,
+        relation    : 'Big Sister' ,
+        location    : 'Bhola' ,
+        phoneNumber : '015384586' ,
+    },
+    {
+        name        : 'MD Niaz' ,
+        relation    : 'Brother' ,
+        location    : 'Singapur' ,
+        phoneNumber : '16852369' ,
+    },
+    {
+        name        : 'MD Achem' ,
+        relation    : 'ME' ,
+        location    : 'Dhaka' ,
+        phoneNumber : '01764849199' ,
+    },
+    {
+        name        : 'Mawlana Abu Taher' ,
+        relation    : 'Father' ,
+        location    : 'Charfation' ,
+        phoneNumber : '01726531186' ,
+    },
+    {
+        name        : 'Hasina Begum' ,
+        relation    : 'Mother' ,
+        location    : 'Charfation' ,
+        phoneNumber : '01720562636' ,
+    },
+]
+
+const developer = [
+    {
+        id          :   2231,
+        name        :   'Achem',
+        skill       :   'Java',
+        loocation   :   'Dhaka',
+        income      :   124000,
+    },
+    {
+        id          :   0225,
+        name        :   'Akash',
+        skill       :   'Django',
+        loocation   :   'Manikgonj',
+        income      :   5865,
+    },
+    {
+        id          :   0225,
+        name        :   'Nahid',
+        skill       :   'Java',
+        loocation   :   'Habigonj',
+        income      :   17836,
+    },
+    {
+        id          :   0225,
+        name        :   'Shorif',
+        skill       :   'Laravel',
+        loocation   :   'Bhola',
+        income      :   8589,
+    },
+    {
+        id          :   0225,
+        name        :   'Mamun',
+        skill       :   'Java',
+        loocation   :   'Shoriotpur',
+        income      :   15589,
+    },
+]
+const gpa = [
+    {
+        id          :    1,
+        name        :   'Jweel',
+        rool        :   25,
+        reg         :   365562544154148,
+        class       :   362,
+        loocation   :   'Dhaka',
+
+        bangla      :   80,
+        agreecuncer :   40,
+        english     :   96,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   80,
+    },
+    {
+        id          :    2,
+        name        :   'Achem',
+        rool        :   30,
+        reg         :   155562543040148,
+        class       :   8,
+        loocation   :   'Bhola',
+
+        bangla      :   50,
+        agreecuncer :   90,
+        english     :   88,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   78,
+    },
+    {
+        id          :    3,
+        name        :   'Rana',
+        rool        :   40,
+        reg         :   225562544542248,
+        class       :   9,
+        loocation   :   'Khulna',
+
+        bangla      :   98,
+        agreecuncer :   98,
+        english     :   63,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   89,
+    },
+    {
+        id          :    4,
+        name        :   'Sweety',
+        rool        :   21,
+        reg         :   95562542948,
+        class       :   7,
+        loocation   :   'Kurigram',
+
+        bangla      :   75,
+        agreecuncer :   60,
+        english     :   48,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   32,
+    },
+    {
+        id          :    5,
+        name        :   'Rayhan',
+        rool        :   41,
+        reg         :   75562544748,
+        class       :   8,
+        loocation   :   'Rangpur',
+
+        bangla      :   93,
+        agreecuncer :   45,
+        english     :   69,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   30,
+    },
+    {
+        id          :    6,
+        name        :   'ashraf',
+        rool        :   23,
+        reg         :   855625423148,
+        class       :   8,
+        loocation   :   'Dinajpur',
+
+        bangla      :   96,
+        agreecuncer :   46,
+        english     :   78,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   45,
+    },
+    {
+        id          :    7,
+        name        :   'Arafat',
+        rool        :   62,
+        reg         :   2555625465148,
+        class       :   7,
+        loocation   :   'Khulna',
+
+        bangla      :   50,
+        agreecuncer :   50,
+        english     :   69,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   63,
+    },
+    {
+        id          :    8,
+        name        :   'Mahi',
+        rool        :   29,
+        reg         :   7355625429148,
+        class       :   9,
+        loocation   :   'Savar',
+
+        bangla      :   48,
+        agreecuncer :   66,
+        english     :   78,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   48,
+    },
+    {
+        id          :    9,
+        name        :   'Nahid',
+        rool        :   52,
+        reg         :   955625452148,
+        class       :   8,
+        loocation   :   'Gazipur',
+
+        bangla      :   59,
+        agreecuncer :   97,
+        english     :   36,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   78,
+    },
+    {
+        id          :    10,
+        name        :   'Sabbir',
+        rool        :   25,
+        reg         :   855625465148,
+        class       :   7,
+        loocation   :   'CGT',
+
+        bangla      :   77,
+        agreecuncer :   89,
+        english     :   36,
+        math        :   69,
+        socialS     :   98,
+        sicence     :   82,
+        religons    :   60,
+        commerce    :   79,
+    },
+]
+
+
+let datajeson = JSON.stringify(phone);
+localStorage.setItem( 'phoneData' , datajeson )
+
+let devjson = JSON.stringify(developer);
+localStorage.setItem( 'developerData' , devjson )
+
+let gpajeson = JSON.stringify( gpa );
+localStorage.setItem( 'gpaData' , gpajeson)
+
+
+
+
